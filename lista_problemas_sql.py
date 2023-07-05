ccbe2752d76ff11329150a3ac029c572eb77116c
CesarOli <cesaraleoliveira@gmail.com>
2023-07-04 23:48:26 -0300

    Adição função sleep e config database.

--- lista_problemas_sql.py
+++ lista_problemas_sql.py
@@ -1,1 +1,9 @@
 import mysql.connector
+from time import sleep
+
+config = { 
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'SENHA_DESEJADA',
+    'database': 'lista_problemas_sql'
+}
